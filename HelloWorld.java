be5cfcc4204b08ef6304cf60238fe6f97da9652d
ss Mo <ilxj20@163.com>
2016-12-02 00:36:44 +0800

    add a method reverString in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,4 +5,19 @@ public class HelloWord {
 	 System.out.println("Hello EveryOne this is My First Project!");
 
     }
+    public static void reverString(){
+    
+	char[] chars = str.toCharArray();
+	System.out.println(chars);
+        for (int i = 0, len = chars.length; i < len / 2; i++) {
+	char temp = chars[i];
+	chars[i] = chars[len - 1 - i];
+	chars[len - 1 - i] = temp;
+	}
+        System.out.println(chars);
+        return;
+   }
+}
+
+    
 }
