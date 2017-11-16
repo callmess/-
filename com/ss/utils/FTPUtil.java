5c189883c5c9f851e3c70f4f8cb98f3493a21f71
sspc <ilxj20@126.com>
2017-11-17 00:57:20 +0800

    add FTPUtil.java

---
+++ com/ss/utils/FTPUtil.java
@@ -0,0 +1,153 @@
+package com.ss.utils;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+
+public class FTPUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(FTPUtil.class);
+
+    private static String ftpIp = PropertiesUtil.getProperty("ftp.server.ip");
+    private static String ftpUser = PropertiesUtil.getProperty("ftp.user");
+    private static String ftpPass = PropertiesUtil.getProperty("ftp.pass");
+    private String ip;
+    private int port;
+    private String user;
+    private String pwd;
+    private FTPClient ftpClient;
+
+    public FTPUtil(String ip, int port, String user, String pwd) {
+        this.ip = ip;
+        this.port = port;
+        this.user = user;
+        this.pwd = pwd;
+    }
+
+    /**
+     * 文件上传
+     * @param fileList 文件列表
+     * @return result
+     * @throws IOException io ex
+     */
+    public static boolean uploadFile(List<File> fileList) throws IOException {
+        FTPUtil ftpUtil = new FTPUtil(ftpIp, 21, ftpUser, ftpPass);
+        logger.info("开始连接ftp服务器");
+        boolean result = ftpUtil.uploadFile("img", fileList);
+        logger.info("开始连接ftp服务器,结束上传,上传结果:{}");
+        return result;
+    }
+
+    /**
+     * 文件上传
+     * @param remotePath 远程路径
+     * @param fileList   文件
+     * @return result
+     * @throws IOException io ex
+     */
+    private boolean uploadFile(String remotePath, List<File> fileList) throws IOException {
+        boolean uploaded = false;
+        FileInputStream fis = null;
+        //连接FTP服务器
+        if (connectServer(this.ip, this.port, this.user, this.pwd)) {
+            try {
+                if (!ftpClient.changeWorkingDirectory(remotePath)) {
+                    //如果目录不存在创建目录
+
+                    if (!ftpClient.makeDirectory(remotePath)) {
+                        return uploaded;
+
+                    } else {
+                        ftpClient.changeWorkingDirectory(remotePath);
+                    }
+                }
+
+                ftpClient.setBufferSize(1024);
+                ftpClient.setControlEncoding("UTF-8");
+                ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+                ftpClient.enterLocalPassiveMode();
+                for (File fileItem : fileList) {
+                    fis = new FileInputStream(fileItem);
+                    //uploaded = ftpClient.storeFile(fileItem.getName(), fis);
+                    uploaded = ftpClient.storeFile(new String(fileItem.getName().getBytes("GBK"), "iso-8859-1"), fis);
+                }
+
+            } catch (IOException e) {
+                logger.error("上传文件异常", e);
+                uploaded = false;
+                e.printStackTrace();
+            } finally {
+                fis.close();
+                ftpClient.disconnect();
+            }
+        }
+        return uploaded;
+    }
+
+    /**
+     * 连接ftp服务器
+     * @param ip   IP地址
+     * @param port 端口
+     * @param user 用户
+     * @param pwd  密码
+     * @return result
+     */
+    private boolean connectServer(String ip, int port, String user, String pwd) {
+
+        boolean isSuccess = false;
+        ftpClient = new FTPClient();
+        try {
+            ftpClient.connect(ip);
+            isSuccess = ftpClient.login(user, pwd);
+        } catch (IOException e) {
+            logger.error("连接FTP服务器异常", e);
+        }
+        return isSuccess;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public FTPClient getFtpClient() {
+        return ftpClient;
+    }
+
+    public void setFtpClient(FTPClient ftpClient) {
+        this.ftpClient = ftpClient;
+    }
+}
