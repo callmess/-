e9cfd0f7ccab795babc9086fde59b63fd77da629
sspc <ilxj20@126.com>
2017-11-17 00:56:56 +0800

    add DateTimeUtil.java

---
+++ com/ss/utils/DateTimeUtil.java
@@ -0,0 +1,78 @@
+package com.ss.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+
+public class DateTimeUtil {
+
+    //joda-time
+
+    //str->Date
+    //Date->str
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+
+    /**
+     * String 转Date
+     * @param dateTimeStr string时间
+     * @param formatStr   时间格式
+     * @return date时间
+     */
+    public static Date strToDate(String dateTimeStr, String formatStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(formatStr);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    /**
+     * Date转String时间
+     * @param date      date时间
+     * @param formatStr 时间格式
+     * @return String时间
+     */
+    public static String dateToStr(Date date, String formatStr) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
+    }
+
+    /**
+     * String 转Date
+     * @param dateTimeStr string时间
+     * @return date时间
+     */
+    public static Date strToDate(String dateTimeStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    /**
+     * Date转String时间
+     * @param date date时间
+     * @return String时间
+     */
+    public static String dateToStr(Date date) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(DateTimeUtil.dateToStr(new Date(), "yyyy-MM-dd HH:mm:ss"));
+        System.out.println(DateTimeUtil.strToDate("2010-01-01 11:11:11", "yyyy-MM-dd HH:mm:ss"));
+
+    }
+
+
+}
